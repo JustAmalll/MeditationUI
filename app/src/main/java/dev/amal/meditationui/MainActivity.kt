1ffd930d871fcc26a986a0071d17069562dd38eb
JustAmalll <amalkrasavchik@gmail.com>
2021-11-08 14:14:04 +0600

    MeditationUI

---
+++ app/src/main/java/dev/amal/meditationui/MainActivity.kt
@@ -0,0 +1,22 @@
+package dev.amal.meditationui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import dev.amal.meditationui.ui.theme.MeditationUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditationUITheme {
+
+            }
+        }
+    }
+}
